543ff1a7a58739dc4081c6bde65e38f68436257f
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2023-05-13 14:50:02 +0200

    test Sismo button

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,18 @@
 import type {FunctionComponent} from "react";
 import Admin from "./admin/Admin";
 import User from "./user/User";
+import UserSismo from "./user/UserSismo";
 
-const App: FunctionComponent = () => (window.location.pathname === '/admin' ? <Admin/> : <User/>)
+const App: FunctionComponent = () => {
+    switch (window.location.pathname) {
+        case '/admin':
+            return <Admin/>;
+        case '/sismo':
+            return <UserSismo/>;
+        case '/':
+            return <User/>;
+    }
+    return null;
+}
 
 export default App;
---
+++ front/src/user/UserSismo.tsx
@@ -0,0 +1,44 @@
+import type {FunctionComponent} from 'react';
+import {useState} from "react";
+import {AuthType, SismoConnectButton, SismoConnectClientConfig} from "@sismo-core/sismo-connect-react";
+
+
+const sismoConnectConfig: SismoConnectClientConfig = {
+    appId: "0xf4977993e52606cfd67b7a1cde717069",
+    devMode: {
+        enabled: true,
+    },
+};
+
+export const signMessage = (account: string) => `Hello ${account}`;
+const UserSismo: FunctionComponent = () => {
+    const [responseBytes, setResponseBytes] = useState<string>("");
+    const [account] = useState<`0x${string}`>(
+        "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+    );
+
+    return (
+        <>
+            <div>{responseBytes}</div>
+            <div>
+                <SismoConnectButton
+                    // the client config created
+                    config={sismoConnectConfig}
+                    // the auth request we want to make
+                    // here we want the proof of a Sismo Vault ownership from our users
+                    auths={[{authType: AuthType.VAULT}]}
+                    // we ask the user to sign a message
+                    // it will be used onchain to prevent front running
+                    signature={{message: signMessage(account)}}
+                    // onResponseBytes calls a 'setResponse' function
+                    // with the responseBytes returned by the Sismo Vault
+                    onResponseBytes={(responseBytes: string) => setResponseBytes(responseBytes)}
+                    // Some text to display on the button
+                    text={"Connect with Sismo"}
+                />
+            </div>
+        </>
+    );
+}
+
+export default UserSismo;
