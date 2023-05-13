d9421d6b945f13dc0b01b05584bf39d70927d52f
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2023-05-13 11:20:08 +0200

    front init

---
+++ front/src/App.tsx
@@ -0,0 +1,7 @@
+import {FunctionComponent} from "react";
+
+const App: FunctionComponent = () => {
+    return <div>G-Spot</div>
+};
+
+export default App;
---
+++ front/src/index.tsx
@@ -0,0 +1,10 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from "./App";
+
+const container = document.getElementById('root');
+if (container) {
+    const root = createRoot(container);
+    root.render(<StrictMode><App/></StrictMode>);
+}
