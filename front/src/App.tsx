54d2737101e5212a2060a34cb034b9517cab47f0
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2023-05-14 07:27:15 +0200

    add menu

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,20 +3,42 @@ import Admin from "./admin/Admin";
 import User from "./user/User";
 import UserSismo from "./user/UserSismo";
 import Worldcoin from './service/Worldcoin'
+import {Container, Nav, Navbar} from "react-bootstrap";
 
-const App: FunctionComponent = () => {
-
+const getComponent = () => {
     switch (window.location.pathname) {
-      case '/free':
-          return <Worldcoin/>;
+        case '/free':
+            return <Worldcoin/>;
         case '/admin':
             return <Admin/>;
         case '/sismo':
             return <UserSismo/>;
         case '/':
             return <User/>;
+        default:
+            return undefined;
     }
-    return null;
+}
+
+const App: FunctionComponent = () => {
+    return (
+        <>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Global spot</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Metamask</Nav.Link>
+                            <Nav.Link href="/free">Worldcoin</Nav.Link>
+                            <Nav.Link href="/admin">Admin</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            {getComponent()}
+        </>
+    );
 }
 
 export default App;
